f1c2eb1b86ad788c84ce743c7c907f76075d9ab5
김석곤 <krdn.net@gmail.com>
2018-04-25 13:50:02 +0900

    Add project files.

---
+++ QuickStartIdentityServer/Config.cs
@@ -0,0 +1,57 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuickStartIdentityServer
+{
+    public class Config
+    {
+        // ** Defining the API
+        // *****************************************************************
+        // 범위는 보호하려는 시스템의 리소스를 정의합니다, 예: APIs.
+
+        // 이 연습에 인 메모리 구성을 사용하고 있기 때문에 API를 추가하기 위해 필요한 것은 
+        // ApiResource 유형의 개체를 만들고 적절한 속성을 설정하는 것입니다.
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+
+
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+
+        // Defining the client 
+        // *******************************************
+        // 다음 단계는이 API에 액세스 할 수있는 클라이언트를 정의하는 것입니다.
+
+        // 이 시나리오의 경우 클라이언트는 대화 형 사용자를 가지지 않으며 IdentityServer와 함께 
+        // 클라이언트 보안을 사용하여 인증합니다.Config.cs 파일에 다음 코드를 추가하십시오.
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+
+                    // no interactive user, use the clientid/secret for authentication
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    // secret for authentication
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }
+}
---
+++ QuickStartIdentityServer/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace QuickStartIdentityServer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // 추가
+            // AddIdentityServer는 DI에 IdentityServer  서비스를 등록합니다. 또한 런타임 상태를 위해 메모리 내 저장소를 등록합니다. 
+            // 이것은 개발 시나리오에 유용합니다. 운영 시나리오에서는 데이터베이스나 캐시 같은 영구 또는 공유 저장소가 필요합니다. 
+            // 자세한 내용은 EntityFramework quickstart를 참조하십시오.
+
+
+            // AddDeveloperSigningCredential 확장은 서명 토큰을위한 임시 키 자료를 만듭니다. 
+            // 다시 말하지만이 방법은 시작하는 데 유용 할 수 있지만 프로덕션 시나리오의 일부 핵심 키 재료로 대체해야합니다. 
+            // 자세한 내용은 암호화 문서(http://docs.identityserver.io/en/release/topics/crypto.html#refcrypto)를 참조하십시오.
+
+
+            // configure identity server with in-memory stores, keys, clients and resources
+            services.AddIdentityServer()
+                .AddDeveloperSigningCredential()
+                .AddInMemoryApiResources(Config.GetApiResources())
+                .AddInMemoryClients(Config.GetClients());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // 추가
+            app.UseIdentityServer();
+
+
+            // 삭제
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
+        }
+    }
+}
